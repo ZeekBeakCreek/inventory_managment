19e39834a81cda8959731b58c7103b6ef430a46d
ZeekBeakCreek <89820141+ZeekBeakCreek@users.noreply.github.com>
2023-02-13 15:27:18 -0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,28 @@
-<?php 
+<?php
+
+
 $servername = 'localhost';
 $username = 'root';
 $password = '';
 $dbname = "main";
 
-// Create connection
+//create connection 
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// create database
+$sql = "CREATE DATABASE $dbname";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully with the name newDB";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+// Create connection if database is created successfully
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
@@ -23,10 +41,9 @@ restock_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
 
 if ($conn->query($sql) === TRUE) {
-  echo "Table inventoryDB created successfully";
+  echo " & Table inventoryDB created successfully";
 } else {
   echo "Error creating table: " . $conn->error;
 }
 
 $conn->close();
-?>
